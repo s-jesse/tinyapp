892c76fe78de3137661838077d168e79334c2232
Jesse Sandhu <s.jesse23@gmail.com>
2023-06-26 11:18:49 -0700

    all helper functions move to this file

--- helpers.js
+++ helpers.js
@@ -1,3 +1,5 @@
+const {urlDatabase} = require("./databases")
+
 const getUserByEmail = function(email, database) {
   let userFound;
   for (const userId in database) {
@@ -9,4 +11,29 @@ const getUserByEmail = function(email, database) {
   return userFound;
 };
 
-module.exports = getUserByEmail;
+// RANDOM STRING ID GENERATOR FUNCTION
+const generateRandomString = function(length = 6) {
+  let randomStr = [];
+  const char = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  for (let i = 0; i < length; i++) {
+    let charIndex = Math.floor(Math.random() * char.length);
+    randomStr.push(char.charAt(charIndex));
+
+  }
+
+  randomStr = randomStr.join("");
+  return randomStr;
+};
+
+// FILTERING URLDATABSE BASE ON SPECIFIC USER ID & URLS CONNECTED WITH SAID USER
+const urlsForUser = function(id, urls) {
+  let usersUrls = {};
+  for (let userUrlId in urls) {
+    if (id === urlDatabase[userUrlId].userID) {
+      usersUrls[userUrlId] = urlDatabase[userUrlId];
+    }
+  }
+  return usersUrls;
+};
+
+module.exports = {getUserByEmail, generateRandomString, urlsForUser};
