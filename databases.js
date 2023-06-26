9aaf8a0c55a1587887f4000be56fde02df9f115e
Jesse Sandhu <s.jesse23@gmail.com>
2023-06-26 11:21:16 -0700

    move database/user object into this file

---
+++ databases.js
@@ -0,0 +1,29 @@
+// USERS OBJECT
+const users = {
+  abc: {
+    id: "abc",
+    email: "a@a.com",
+    password: "123",
+  },
+  def: {
+    id: "def",
+    email: "b@b.com",
+    password: "456",
+  },
+};
+
+//////////////////////////////////////////
+
+// URLDATABSE OBJECT
+const urlDatabase = {
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW",
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+  },
+};
+
+module.exports = {users, urlDatabase}
